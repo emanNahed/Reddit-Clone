a8726429a171ec11e4abb70df4a4ecec1cb10c84
EmanAmassi <eman.amassi98@gmail.com>
2021-09-01 02:06:21 +0300

    create database table, build it's connection'

---
+++ server/database/config/connection.js
@@ -0,0 +1,12 @@
+const {Pool} = require('pg');
+
+const dbUrl= process.env.DB_URL;
+
+const options = {
+    connectionString: dbUrl,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
+
+module.exports = new Pool(optins);
